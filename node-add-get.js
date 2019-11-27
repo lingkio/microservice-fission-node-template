bd593a71de7d81d3993354cbf743c7ab9f05fde6
Harjinder Dhanjal <malotian@gmail.com>
2019-11-27 15:20:02 +0000

    reformating

--- node-add-get.js
+++ node-add-get.js
@@ -1,13 +1,13 @@
 var url = require('url');
 
-module.exports = async function(context) {
+module.exports = async function (context) {
     var url_parts = url.parse(context.request.url, true);
     var query = url_parts.query;
-    var n1=parseInt(query.number1);
-    var n2=parseInt(query.number2);   
+    var n1 = parseInt(query.number1);
+    var n2 = parseInt(query.number2);
     return {
         status: 200,
-        body:"total : "+ JSON.stringify(n1+n2)
+        body: "total : " + JSON.stringify(n1 + n2)
     }
 }
 
--- node-add-post.js
+++ node-add-post.js
@@ -1,13 +1,13 @@
-module.exports = async function(context) {
+module.exports = async function (context) {
 
-const stringBody = JSON.stringify(context.request.body);
-const body = JSON.parse(stringBody);
-const json = body.numbers;
-var n1=parseInt(JSON.stringify(json[0].number1));
-var n2=parseInt(JSON.stringify(json[1].number2));
-   return {
-        status: 200,
-        body:"total : "+ JSON.stringify(n1+n2)
-	};
+        const stringBody = JSON.stringify(context.request.body);
+        const body = JSON.parse(stringBody);
+        const json = body.numbers;
+        var n1 = parseInt(JSON.stringify(json[0].number1));
+        var n2 = parseInt(JSON.stringify(json[1].number2));
+        return {
+                status: 200,
+                body: "total : " + JSON.stringify(n1 + n2)
+        };
 }
 

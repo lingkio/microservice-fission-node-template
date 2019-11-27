b9d40e568e12c6c5cea2823977d51783ca3b7313
Harjinder Dhanjal <malotian@gmail.com>
2019-11-27 15:30:44 +0000

    refactorization

--- node-add-post.js
+++ node-add-post.js
@@ -3,8 +3,8 @@ module.exports = async function (context) {
         const stringBody = JSON.stringify(context.request.body);
         const body = JSON.parse(stringBody);
         const json = body.numbers;
-        var n1 = parseInt(JSON.stringify(json[0].number1));
-        var n2 = parseInt(JSON.stringify(json[1].number2));
+        var n1 = parseInt(JSON.stringify(json[0].number));
+        var n2 = parseInt(JSON.stringify(json[1].number));
         return {
                 status: 200,
                 body: "total : " + JSON.stringify(n1 + n2)
